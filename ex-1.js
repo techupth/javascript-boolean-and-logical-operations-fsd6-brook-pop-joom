5264f569e2bbe915b732e4baf046d543efe2cef8
Tynnys1120 <53811329+708799@users.noreply.github.com>
2024-05-09 15:36:43 +0700

    js: assignment 4

--- ex-1.js
+++ ex-1.js
@@ -4,5 +4,8 @@ let hasCriminalBlacklist;
 let isAllow;
 
 // Start coding here
-
+isOver18 = false;
+hasCriminalBlacklist = true;
+isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
+//false ไม่มีสิทธิ์

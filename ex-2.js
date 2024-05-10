e58e0470d385691f9ae0340cb9a65aaa75374551
Pop <shounen.pop@gmail.com>
2024-05-10 10:37:18 +0700

    Update : ex2

--- ex-2.js
+++ ex-2.js
@@ -1,17 +1,37 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000 = true;
-let isWeekday = true;
-let hasBoughtProductFromITCategory = false;
-let hasAttendedDiscountEvent = true;
-let isPlatinum = false;
+let lastMonthPaidMoreThan4000 = 4000;
+let isWeekday = {
+  Monday: true,
+  Tuesday: true,
+  Wednesday: true,
+  Thursday: true,
+  Friday: true,
+  Saturday: false,
+  Sunday: false,
+};
+
+let hasBoughtProductFromITCategory = {
+  has: true,
+  not: false,
+};
+
+let hasAttendedDiscountEvent = {
+  has: true,
+  not: false,
+};
+
+let isPlatinum = {
+  Platinum: true,
+  Gold: false,
+};
 
 let hasPromotion =
-  lastMonthPaidMoreThan4000 &&
-  isWeekday &&
-  hasBoughtProductFromITCategory &&
-  hasAttendedDiscountEvent &&
-  isPlatinum;
+  lastMonthPaidMoreThan4000 < 4001 &&
+  isWeekday.Friday &&
+  hasBoughtProductFromITCategory.not &&
+  hasAttendedDiscountEvent.has &&
+  isPlatinum.Gold;
 
-console.log(hasPromotion);
+console.log(lastMonthPaidMoreThan4000 < 4001);

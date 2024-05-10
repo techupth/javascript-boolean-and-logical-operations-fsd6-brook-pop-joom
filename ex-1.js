0ad21f528d7ceac45ed95b22b2b81ef68036e41b
Pop <shounen.pop@gmail.com>
2024-05-10 08:27:43 +0700

    Update : ex1-3

--- ex-1.js
+++ ex-1.js
@@ -4,5 +4,9 @@ let hasCriminalBlacklist;
 let isAllow;
 
 // Start coding here
+isOver18 = 18;
+hasCriminalBlacklist = false;
+
+isAllow = isOver18 > 18 && hasCriminalBlacklist == false;
 
 console.log(isAllow);
